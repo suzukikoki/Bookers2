a327c51e3e9ae5227c953207b8a9a01370e16c12
suzukikoki <114800671+suzukikoki@users.noreply.github.com>
2023-02-20 14:44:20 +0000

    [7]

---
+++ java2/sample2_7.java
@@ -0,0 +1,9 @@
+class sample2_7 {
+  public static void main(String[] args) {
+    int a = 10;
+    int b = a;
+    System.out.println("a:" + a + "b:" + b);
+    b = 50;
+    System.out.println("a:" + a + "b:" + b);
+  }
+}
