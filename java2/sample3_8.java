aed15091c87fb4a5fbe86d6dcd5dbb886663c479
suzukikoki <114800671+suzukikoki@users.noreply.github.com>
2023-03-01 07:24:42 +0000

    [8]

---
+++ java2/sample3_8.java
@@ -0,0 +1,11 @@
+class sample3_8 {
+  public static void main(String[] args) {
+    int num = 20;
+    if (num < 10) {
+      System.out.println("numnの値は10未満です");
+    } else {
+      System.out.println("numの値は10以上です");
+    }
+    System.out.println("if-else文の外の処理");
+  }
+}
