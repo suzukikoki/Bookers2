b6eedf08463df1b9064b0d433d7c9cea681e60ec
suzukikoki <114800671+suzukikoki@users.noreply.github.com>
2023-03-04 11:26:57 +0000

    [10]

---
+++ java2/sample3_10.java
@@ -0,0 +1,10 @@
+class sample3_10 {
+  public static void main(String[] args) {
+    int num = 20;
+    String str = "numの値は";
+    str += num < 10 ?"10未満":"10以上";
+    System.out.println(str);
+  }
+}
+
+//条件式? true:false;
