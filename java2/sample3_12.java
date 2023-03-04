966b1f43a9eeca6b7550d55529f44c9a6a08e009
suzukikoki <114800671+suzukikoki@users.noreply.github.com>
2023-03-04 11:40:33 +0000

    [12]

---
+++ java2/sample3_12.java
@@ -0,0 +1,15 @@
+class sample3_12 {
+  public static void main(String[] args) {
+    int num =2;
+    switch(num) {
+      case 1:
+        System.out.println("numの値は１です");
+        break;
+      case 2:
+        System.out.println("numの値は２です");
+        //break;
+      default:
+        System.out.println("defaultです");
+    }
+  }
+}
