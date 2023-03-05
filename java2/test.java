c313b694f81057d25fd8d110bdb29464af0cd853
suzukikoki <114800671+suzukikoki@users.noreply.github.com>
2023-03-05 13:17:36 +0000

    [test]

---
+++ java2/test.java
@@ -0,0 +1,10 @@
+class test {
+  public static void main(String[] args) {
+    int a = 2;
+    int b = 4;
+    System.out.println((10 + 3 * 2) + "");
+    System.out.println(++a * b++); 
+    System.out.println(++a);
+    System.out.println(a++);
+  }
+}
