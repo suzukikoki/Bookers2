17e778a0e7a1bac7f1d4db93ca502a7a3241d454
suzukikoki <114800671+suzukikoki@users.noreply.github.com>
2023-03-05 13:29:00 +0000

    [1]

---
+++ java2/sample4_1.java
@@ -0,0 +1,10 @@
+class sample4_1 {
+  public static void main(String[] args) {
+    int num = 0;
+    
+    while (num < 5) {
+      System.out.println("numの値は" + num + "です");
+      num++;
+    }
+  }
+}
