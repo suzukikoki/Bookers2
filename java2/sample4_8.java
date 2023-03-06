9dd86eecf17d67b0bd3eaf016f0ecb9727138272
suzukikoki <114800671+suzukikoki@users.noreply.github.com>
2023-03-06 13:37:03 +0000

    [8]

---
+++ java2/sample4_8.java
@@ -0,0 +1,10 @@
+class sample4_8 {
+  public static void main(String[] args) {
+    
+    for(int i = 1; i < 10; i++) {
+      if((i % 4) == 0) {
+        System.out.println(i + "は4の倍数です");
+      }
+    }
+  }
+}
