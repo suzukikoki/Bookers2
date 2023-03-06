bcd595ecbb55a9177197d0fe202328e9799a1352
suzukikoki <114800671+suzukikoki@users.noreply.github.com>
2023-03-06 09:22:32 +0000

    [6]

---
+++ java2/sample4_6.java
@@ -0,0 +1,13 @@
+class sample4_6 {
+  public static void main(String[] args) {
+    int count1 = 0;
+    for(; count1 < 5; count1++) {//
+    
+    System.out.println(count1);
+  }
+  System.out.println();
+  for(int count2 = 0; count2 < 5;) {//
+  System.out.println(count2++);
+}
+}
+}
