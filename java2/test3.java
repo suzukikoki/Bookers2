fa7ae67c63faa20d52dbc5b6601d2fedb6fb8820
suzukikoki <114800671+suzukikoki@users.noreply.github.com>
2023-03-09 16:33:59 +0000

    [3]

---
+++ java2/test3.java
@@ -0,0 +1,24 @@
+// class test3 {
+//   public static void main(String[] args) {
+//     int x = 4;
+//     do {
+//       ++x;
+//       System.out.print(x + "");
+//     } while (x == 5);
+//     System.out.println();
+//   }
+// }
+
+
+class test3 {
+    public static void main(String[] args) {
+        System.out.print("難しいことは");
+        System.out.println("私に聞くな");
+        System.out.print("金と女のことは");
+        System.out.println("私に聞くな");
+    }
+}
+
+
+// System.out.println は、System.out.print + 改行 と同様の動きをする
+// System.out.println は最後尾に改行を付与するが、先頭には改行を付与しない
