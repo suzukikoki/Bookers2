e928ab5594ef1d1d43f179254c658f6ef8214855
suzukikoki <114800671+suzukikoki@users.noreply.github.com>
2023-03-13 14:07:38 +0000

    [4] [4]

---
+++ java2/sample6_4.java
@@ -0,0 +1,7 @@
+class test {
+  String myPrint(int a, String b) {return "test";}
+  String myPrint(int a, int b) {return "test";}
+  String mtPrint(String a, int b) {return "test";}
+  //void myPrint(int a, String b) {}
+  //String myPrint(int x, String y) {return "test";}
+}
