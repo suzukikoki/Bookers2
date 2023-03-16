13358830d8cede0eb7cc58ef657cc6ad6df0bf95
suzukikoki <114800671+suzukikoki@users.noreply.github.com>
2023-03-16 14:04:41 +0000

    [6]

---
+++ java2/sample6_6.java
@@ -0,0 +1,20 @@
+class Test {
+  int instanceVal = 100;
+  static int staticVal = 200;
+  void methodA() {System.out.println("methodA():" + instanceVal);}
+  static void methodB() {
+    System.out.println("methodB():" + staticVal);}
+}
+
+class sample6_6 {
+  public static void main(String[] args) {
+    System.out.println(Test.staticVal);
+    Test.methodB();
+    System.out.println("-------------");
+    Test t = new Test();
+    System.out.println(t.instanceVal);
+    System.out.println(t.staticVal);
+    t.methodA();
+    t.methodB();
+  }
+}
